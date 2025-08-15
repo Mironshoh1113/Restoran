2e0fda54a82d13236a2010dd2f5ba86847320e8f
mironshoh1113 <miromironshoh1113@gmail.com>
2025-08-15 09:52:42 +0500

    ishhona

--- resources/views/web-interface/index.blade.php
+++ resources/views/web-interface/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="uz" class="shrink-30">
+<html lang="uz" class="shrink-50">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
